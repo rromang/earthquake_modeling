5a2975ad6c32819635a3a3c878907f243901ff91
rromang <80008461+rromang@users.noreply.github.com>
2022-01-25 21:17:58 -0700

    finished app, created json file equake dict, created htmls

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, MetaData, inspect, Table
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+from flask import url_for
+import json
 
+
+# #################################################
+# # Flask Setup
+# #################################################
+app = Flask(__name__, static_url_path='/static/')
 
 #################################################
 # Database Setup
@@ -29,44 +36,33 @@ Earthquakes = Base.classes.earthquake_raw
 # print(Earthquakes)
 
 # #################################################
-# # Flask Setup
-# #################################################
-app = Flask(__name__)
-
-
-# #################################################
 # # Flask Routes
 # #################################################
+with open('Database/json/earthquake.json', 'r') as myfile:
+    data = myfile.read()
 
 @app.route("/")
 def welcome():
     # """List all available api routes."""
-    return (
-        f"Welcome to Earthquake Information API!</br>"
-        f"Available Routes:<br/>"
-        f"/api/v1.0/earthquakes<br/>"
-        )
+    return render_template('home.html')
         
-
-
-@app.route("/api/v1.0/earthquakes")
+@app.route("/earthquakes")
 def earthquakes():
-    # Create our session (link) from Python to the DB
     session = Session(engine)
 
     """Return a list of all earthquake information"""
-    # Query all measurements
+        # Query all measurements
     results = session.query(Earthquakes.time, Earthquakes.mag, Earthquakes.latitude, Earthquakes.longitude, \
-        Earthquakes.depth, Earthquakes.magType, Earthquakes.nst, Earthquakes.gap, Earthquakes.dmin, \
-            Earthquakes.rms, Earthquakes.net, Earthquakes.id, Earthquakes.updated).all()
+            Earthquakes.depth, Earthquakes.magType, Earthquakes.nst, Earthquakes.gap, Earthquakes.dmin, \
+                Earthquakes.rms, Earthquakes.net, Earthquakes.id, Earthquakes.updated).all()
 
     session.close()
 
-    # Convert list of tuples into normal list
+        # Convert list of tuples into normal list
     all_earthquakes = []
     for time, mag, lat, lon, depth, magType, nst, gap, dmin, rms, net, id_equake, updated in results:
         equake_dict = {}
-        
+            
         equake_dict['mag'] = mag
         equake_dict['latitude'] = lat
         equake_dict['longitude'] = lon
@@ -81,7 +77,7 @@ def earthquakes():
         equake_dict['id'] = id_equake
         equake_dict['updated'] = updated
         all_earthquakes.append(equake_dict)
-    
+        
     return jsonify(all_earthquakes)
 
 
